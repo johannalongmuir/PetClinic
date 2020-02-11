8123b22c08ddab5be07732a731e9635e1a1f7abf
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-11 09:41:57 +0000

    Add find by id controller as practice - Off topic

--- petclinic-web/src/main/java/com/longmuir/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/com/longmuir/petclinic/controllers/VetController.java
@@ -4,6 +4,7 @@ import com.longmuir.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/vets")
 @Controller
@@ -22,6 +23,9 @@ public class VetController {
     }
 
 
-
-
+    @RequestMapping("/findbyid")
+    public String getAVet(@RequestParam(value = "id", defaultValue = "-1") String id, Model model){
+        model.addAttribute("vets", vetService.findById(Long.parseLong(id)));
+        return "vets/index";
+    }
 }
