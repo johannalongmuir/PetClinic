6f7579e0782a90ac56d147cd15030fb31814294f
Johanna Longmuir <johannalongmuir@gmail.com>
2020-02-25 15:04:33 +0000

    Closes #28. Add contact info for owner

--- petclinic-data/src/main/java/com/longmuir/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/com/longmuir/petclinic/model/Owner.java
@@ -1,17 +1,17 @@
 package com.longmuir.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@Setter
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
