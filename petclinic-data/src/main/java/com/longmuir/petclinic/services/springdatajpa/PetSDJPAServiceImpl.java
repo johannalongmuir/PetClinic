312362def1a494f5e491e87a37c979f6f410f418
Johanna Longmuir <johannalongmuir@gmail.com>
2020-04-02 14:38:13 +0100

    Closes #43. Add Pet JPA service details

---
+++ petclinic-data/src/main/java/com/longmuir/petclinic/services/springdatajpa/PetSDJPAServiceImpl.java
@@ -0,0 +1,46 @@
+package com.longmuir.petclinic.services.springdatajpa;
+
+import com.longmuir.petclinic.model.Pet;
+import com.longmuir.petclinic.repositories.PetRepository;
+import com.longmuir.petclinic.services.PetService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@Profile("springdatajpa")
+@Service
+public class PetSDJPAServiceImpl implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
